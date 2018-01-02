9482ff330e4a113a8409f71c02d6b554707f1d00
Harrison <hmcguire13@gmail.com>
2018-01-02 08:34:24 -0500

    added styled-components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,23 @@
 import Layout from '../components/Layout.js'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 
 export default () => (
-    <Layout>
-       <p>This is the about page</p>
-    </Layout>
+    <Wrapper>
+    <Title>
+      Hello World, this is my first styled component!
+    </Title>
+  </Wrapper>
 )
