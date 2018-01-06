a350c5a2c84bc881672a904e190290927e58fd3b
Harrison <hmcguire13@gmail.com>
2018-01-06 13:27:27 -0500

    removded lazy load from intro pics

--- components/Intro.js
+++ components/Intro.js
@@ -13,21 +13,15 @@ const Intro = () =>
         <div className="wrapper">
 
             <div className="one">
-                <LazyLoad height={306} offset={300}>
-                    <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/w_800/v1515075026/harrison-mcguire_qq3ixp.jpg" alt="Harrison McGuire"/>
-                </LazyLoad>
+                <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/w_800/v1515075026/harrison-mcguire_qq3ixp.jpg" alt="Harrison McGuire"/>
                 <h3>Hello Galaxy</h3>
                 <p>Hi, I'm Harrison McGuire and I am a web and game developer. My mission is to enrich the user experience with attention to detail and beautiful design. I excel in making modern, performant apps. I dedicate most of my time to JavaScript and Unreal Engine 4.</p>
             </div>
 
-            <LazyLoad height={612} offset={300}>
-                <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/w_800/v1515064571/harrison-mcguire_qjwj6l.jpg" className="two" alt="Harrison McGuire"/>
-            </LazyLoad>
+            <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/w_800/v1515064571/harrison-mcguire_qjwj6l.jpg" className="two" alt="Harrison McGuire"/>
 
             <div className="three">
-                <LazyLoad height={306} offset={300}>
-                    <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/v1515075026/harrison-mcguire-profile_i3jrxr.jpg" alt="Harrison McGuire"/>
-                </LazyLoad>
+                <img src="https://res.cloudinary.com/dz09rnbhe/image/upload/v1515075026/harrison-mcguire-profile_i3jrxr.jpg" alt="Harrison McGuire"/>
                 <h3>Personal Info</h3>
                 <p><span>Name:</span> Harrison McGuire</p>
                 <p><span>Age:</span> {calculateAge(birthday)}</p>
